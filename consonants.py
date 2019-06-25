aee2cca5c6582bf474400b926d1aeee910db485a
db2398 <51949608+db2398@users.noreply.github.com>
2019-06-25 15:31:37 +0530

    Create consonants.py

---
+++ consonants.py
@@ -0,0 +1,11 @@
+l=input()
+v=['a','e','i','o','u','A','E','I','O','U']
+c=['b','c','d','f','g','h','j','k','l','m','n','p','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','R','S','T','V','W','X','Y','Z']
+if l in v:
+      print("Vowel")
+elif l in c:
+     print("Consonant")
+else:
+     print("invalid")
+     
+     
